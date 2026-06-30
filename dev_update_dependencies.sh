adf3e14d2a5dbd0c8a8b70f99bcd9a295a157964
Rob Stradling <rob@sectigo.com>
2026-06-30 09:46:53 +0100

    Regenerate optimal_parents.csv as part of the dev dependency update check.

--- dev_update_dependencies.sh
+++ dev_update_dependencies.sh
@@ -3,3 +3,7 @@
 # Update "dev" Go dependencies.
 go get -modfile=dev_go.mod -u
 go mod tidy -modfile=dev_go.mod
+
+# Run the find_optimal_parents.sh script to update optimal_parents.csv
+SCRIPT_DIR=`cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd`
+$SCRIPT_DIR/find_optimal_parents.sh
